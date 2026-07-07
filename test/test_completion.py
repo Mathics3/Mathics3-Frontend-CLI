39347bf22bf89eb5adc6f49515111e36e250291f
rocky <rb@dustyfeet.com>
2026-07-07 01:35:46 +0000

    Skip completion test if GNU Readline is not available

--- test/test_completion.py
+++ test/test_completion.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 
+import pytest
 from mathics.core.definitions import Definitions
-from mathicsscript.termshell_gnu import TerminalShellGNUReadline
 
-try:
-    __import__("readline")
-except ImportError:
-    have_readline = False
-else:
-    have_readline = True
+pytest.importorskip("readline")
+
+from mathicsscript.termshell_gnu import TerminalShellGNUReadline
 
 
 def test_completion_gnu():
@@ -23,12 +20,7 @@ def test_completion_gnu():
     for prefix, completions in (("Fibonac", "Fibonacci"), ("Adfafdsadfs", None)):
         assert term.complete_symbol_name(prefix, state=0) == completions
 
-    if have_readline:
-        for prefix, completions in (("\\[Alph", "\\[Alpha]"), ("\\[Adfafdsadfs", None)):
-            assert term.complete_symbol_name(prefix, state=0) == completions
+    for prefix, completions in (("\\[Alph", "\\[Alpha]"), ("\\[Adfafdsadfs", None)):
+        assert term.complete_symbol_name(prefix, state=0) == completions
 
     # TODO: multiple completion items
-
-
-if __name__ == "__main__":
-    test_completion_gnu()
